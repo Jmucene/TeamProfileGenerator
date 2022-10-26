e2575a7ca7d87e85c39770c769765da078673774
Jase Mucene <105563420+Jmucene@users.noreply.github.com>
2022-10-26 16:21:53 -0400

    Add files via upload

---
+++ utils/expect.js
@@ -0,0 +1,11 @@
+const expectToBe = (obj, value) => {
+    expect(obj).toBe(value);
+};
+
+const expectStr = (obj, substr) => {
+    expect(obj).toEqual(expect.stringContaining(substr));
+};
+
+const expectNum = obj => {
+    expect(obj).toEqual(expect.any(Number));
+};
